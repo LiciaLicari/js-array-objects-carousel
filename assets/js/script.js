478d0d0e46c87222957a5e675c8a0aeb13e0758c
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-07 19:00:23 +0200

    add next

--- assets/js/script.js
+++ assets/js/script.js
@@ -257,10 +257,17 @@ nextEl.addEventListener('click', function () {
 
 
 prevEl.addEventListener('click', autoScrollPrev);
+nextEl.addEventListener('click', autoScrollNext);
 
 
 function autoScrollPrev() {
     setInterval(prevPic, 2000);
+}
+
+function autoScrollNext() {
+
+    setInterval(nextPic, 2000);
+
 }
 
 function prevPic() {
@@ -303,6 +310,49 @@ function prevPic() {
     nextThumb.classList.add('active');
 
 }
+
+function nextPic(){
+    console.log('cliccato su next');
+    // select the current slide
+    const currentSlide = slidesImages[activeSlide];
+    //console.log(currentSlide);
+    // remove the active class from the current slide
+    currentSlide.classList.remove('active')
+    
+    // select the active thumb
+    const currentThumb = document.querySelector('.thumbnails > img.active')
+    //console.log(currentThumb);
+    // remove the active class from the active thumb
+    currentThumb.classList.remove('active')
+    
+    
+    // activeSlide = 4
+    
+    if (activeSlide === slidesImages.length - 1) {
+        activeSlide = 0
+        // activeSlide = 5
+    } else {
+        // increment the activeSlide of 1
+        activeSlide++
+    }
+    
+    
+    // select the next slide
+    const nextSlide = slidesImages[activeSlide]
+    //console.log(nextSlide);
+    // add the active class to the next slide
+    nextSlide.classList.add('active');
+    
+    //TODO
+    
+    // select the next thumb
+    const nextThumb = document.querySelectorAll('.thumb')[activeSlide]
+    //console.log(nextThumb);
+    // add to the next thumb the active class
+    nextThumb.classList.add('active')
+
+  }
+
 
 
 
